a352c2b14d868cc0a2c136805457f926d0fabcbf
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2023-10-30 15:47:15 +0100

    pusheen

--- src/App.tsx
+++ src/App.tsx
@@ -2,54 +2,48 @@ import './App.css'
 import { ChangeEvent, useState } from 'react'
 import { FruitList } from './components/fruit-list'
 import { FruitSearch } from './components/fruit-search'
-import { listFunctions } from './components/hooks/list-functions';
+
+const fruitArray = [
+  'apple', 
+  'orange', 
+  'banana', 
+  'mango', 
+  'grapefruit', 
+  'coconut', 
+  'strawberry', 
+  'blueberry',
+  'walnut'
+]
 
 function App() {
 
-  const fruitArray = [
-    'apple', 
-    'orange', 
-    'banana', 
-    'mango', 
-    'grapefruit', 
-    'coconut', 
-    'strawberry', 
-    'blueberry',
-    'walnut'
-]
+const [input, setInput] = useState('')
 
-function fruitFinder(input:string){
-
-  fruitArray.forEach(fruit => {
-      if (fruit.includes(input.toLowerCase())) {
-          console.log(fruit);               
-          return fruit
-                
-      }  
-  });
+function fruitFinder(array: string[], input:string){
+  array = fruitArray
+  const filteredFruit = fruitArray.filter((fruit) => fruit.includes(input.toLowerCase()))
+  console.log('Frutta filtrata', filteredFruit);
+  return filteredFruit;
 
 }
 
   function onChange(event: ChangeEvent<HTMLInputElement>){
     const userInput = event.currentTarget.value;
     setInput(userInput)
-    //listMethods.actions.fruitFinder(input)
+    fruitFinder(fruitArray, userInput)
   }
-
-  const [input, setInput] = useState('')
-
 
 
   return (
     <>
       <FruitSearch
       search={onChange}
-      print={fruitFinder(input)}
+      print={fruitFinder(fruitArray, input)}
       />
 
       <FruitList 
-      fruitArray={fruitArray}/>  
-      
+      fruitArray={fruitFinder(fruitArray, input)}
+      />  
     </>
   )
 }
